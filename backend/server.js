8f3967bad534f5f88d51e255a145f9137e5e54a3
nkrisk-17 <nanditha.krishnakumar@btech.christuniversity.in>
2026-03-04 22:54:25 +0530

    Add server port configuration and listener

--- backend/server.js
+++ backend/server.js
@@ -303,4 +303,10 @@ app.listen(PORT, () => {
   console.log("=".repeat(60));
 });
 
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
 module.exports = app;
